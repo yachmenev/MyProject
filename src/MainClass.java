c23324d798e7d83a729321ee15b4f56f4daa22bc
yachmenev <yachmenevev@gmail.com>
2014-08-13 21:42:36 +0300

    Added new class "MyString" and implemented it in main class.

--- src/MainClass.java
+++ src/MainClass.java
@@ -4,8 +4,11 @@ public class MainClass {
 	public static void main(String[] args) {
 		System.out.println("Hello it's a Main Class of my project");
 		MyArray arr = new MyArray();
-		arr.create(5);
+		arr.create(2);
 		arr.show();
+		
+		MyString str = new MyString("My string");
+		str.show();
 	}
 
 }
---
+++ src/MyString.java
@@ -0,0 +1,20 @@
+
+public class MyString {
+	String str;
+	public MyString(){
+		this.str = "1";
+	}
+	public MyString(String str){
+		this.str = str;
+	}
+	
+	public void show(){
+		System.out.println(str);
+	}
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+	
+}
